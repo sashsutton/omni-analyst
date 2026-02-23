0d1bc08a6e5237825d5cff12a7ccca7d9926267a
sashasutton <sashasuttons3@gmail.com>
2026-02-23 13:06:50 +0100

    Deleted app.py and solved the response display

---
+++ README.md
@@ -0,0 +1,14 @@
+# Omni-Analyst: Autonomous Deep-Search & Data visualisation
+
+Omni-Analyst is a agentic AI application built with the **Mistral AI SDK**. It transforms complex natural language queries into polished data reports by autonomously searching the web, executing Python code to generate visualisations, and synthesizing findings.
+
+
+## 🚀 Key Features
+* **Web Search Integration**: Uses Mistral's `web_search` connector to bypass training data cutoffs and fetch real-time market data.
+* **Sandboxed Code Execution**: Leverages the `code_interpreter` to write and run Python code (Pandas/Matplotlib) for data cleaning and visualization.
+* **Automated Artifact Handling**: Automatically downloads and saves generated charts (PNGs) and research summaries locally.
+
+
+
+
+
--- main.py
+++ main.py
@@ -20,11 +20,31 @@ def main():
         inputs=[{"role": "user", "content": user_query}]
     )
 
+    full_report = ""
 
+    for output in response.outputs:
+        if hasattr(output, 'content'):
+            for chunk in output.content:
 
-    print("\n Final report: ")
-    print(response)
+                if chunk.type == 'text':
+                    full_report += chunk.text
 
+                if chunk.type == 'tool_file':
+                    print(f"\n Found chart: {chunk.file_name} (ID: {chunk.file_id})")
+
+                    file_response = client.files.download(file_id=chunk.file_id)
+
+                    file_response.read()
+
+                    save_path = os.path.join("exports/charts", chunk.file_name)
+
+                    with open(save_path, "wb") as f:
+                        f.write(file_response.content)
+
+                    print(f" Chart successfully saved to {save_path}")
+
+    print("\n--- FINAL REPORT ---")
+    print(full_report)
 
 
 
